1cf94f9b500476b84df02cba9ca24193523d8f34
V-atz <104814460+V-atz@users.noreply.github.com>
2023-07-27 07:31:34 +0530

    Add files via upload

---
+++ all patterns/pattern1square.cpp
@@ -0,0 +1,35 @@
+/*
+
+*******
+*******
+*******
+*******
+*******
+*******
+*******
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int i = 1, n;
+    cout << "Enter an integer : ";
+    cin >> n;
+
+    while ( i <= n ) {
+
+        int j = 1;
+        while ( j <= n ) {
+            cout << "*";
+            j ++;
+        }
+
+        cout << endl;
+        i++;
+    } 
+    return 0;
+
+}
